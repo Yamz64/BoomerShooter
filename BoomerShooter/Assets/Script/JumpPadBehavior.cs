0510cc675e88d5e93b845f5258541502dcdea8e2
James Lillicrap <jamesplillicrap@gmail.com>
2022-04-18 05:34:43 -0400

    ~Many many changes
    +Finished Colloseum map
    > created level geo
    > created materials
    > created props
    
    +Added Jump pads
    
    +Started work on voice lines

---
+++ BoomerShooter/Assets/Script/JumpPadBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPadBehavior : MonoBehaviour
+{
+    public float launch_power;
+    private Transform origin;
+    private Transform end;
+
+    private void Start()
+    {
+        origin = transform.GetChild(2);
+        end = transform.GetChild(3);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            other.GetComponent<Rigidbody>().velocity = (end.position - origin.position) *launch_power;
+        }
+    }
+}
